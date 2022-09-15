8625282c98d25a25da9e7c5a2e577c180dd51f34
BurxonCoder <nnburxon@gmail.com>
2022-09-15 12:13:13 +0500

    amazon-api

---
+++ src/components/card.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Cards = ({ item, handleClick }) => {
+    const { title, author, price, img } = item;
+    return (
+        <div className="cards">
+            <div className="image_box">
+                <img src={img} alt="" />
+            </div>
+            <div className="details">
+                <p>{title}</p>
+                <p>{author}</p>
+                <p>Price - {price}Sum</p>
+                <button onClick={() => handleClick(item)}>Sotib olish</button>
+            </div>
+        </div>
+    );
+};
+
+export default Cards;
+
